08d20acd0101421a25a553635c69338f81d3c6c3
Lobniibuyem <gabriellucasteus2@gmail.com>
2026-02-20 22:36:26 -0300

    Create AppleHub.lua

---
+++ AppleHub.lua
@@ -0,0 +1,4 @@
+local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
+if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 or UniverseID == 994732206 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-kaitunBF.lua"))()
+end
