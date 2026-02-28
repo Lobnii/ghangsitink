14e0f8c83b96338aa2103a850e193df7e7699fdd
Lobniibuyem <gabriellucasteus2@gmail.com>
2026-02-28 14:27:28 -0300

    Update AppleHub.lua

--- AppleHub.lua
+++ AppleHub.lua
@@ -1,11 +1,18 @@
-local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
-if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/BSS-AppleCat.lua"))()
+local UniverseID
+local success, result = pcall(function()
+    return game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
+end)
+if success then
+    UniverseID = result
+else
+    UniverseID = nil 
 end
-if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 or UniverseID == 994732206 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-kaitunBF.lua"))()
+if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/BSS-AppleCat.lua"))()
+elseif game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 or UniverseID == 994732206 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-kaitunBF.lua"))()
 elseif game.PlaceId == 10260193230 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-Memesea.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-Memesea.lua"))()
 elseif game.PlaceId == 16732694052 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/Fisch.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/Fisch.lua"))()
 end
