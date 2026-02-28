0211954ffa6df75de286bb9ba51b22363b3baf92
Lobniibuyem <gabriellucasteus2@gmail.com>
2026-02-28 14:58:40 -0300

    Update AppleHub.lua

--- AppleHub.lua
+++ AppleHub.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 1537690962 or game.PlaceId == 4079902982 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/BSS-AppleCat.lua"))()
 end
 if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 122478697296975 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-kaitunBF.lua"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/BF-AppleCatOld.lua"))()
 elseif game.PlaceId == 10260193230 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/Lobnii/ghangsitink/refs/heads/main/AppleCat-Memesea.lua"))()
 elseif game.PlaceId == 16732694052 then
